02d101853ac6b619cb0d4e0806377ff0e2ef529d
NazaninHosseini <nicedesign1990@gmail.com>
2025-12-29 17:30:43 +0330

    add btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,6 @@ import "./App.css";
 
 import Table from "./components/Table";
 
-function App() {
-  return (
-    <div>
-      <h2>خرید معتبرترین ارزهای بازار کریپتو</h2>
-      <Table />
-    </div>
-  );
-}
-
 import CryptoTable from "./components/CryptoTable";
 
 function App() {
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button({ children, onClick, type = "button" }) {
+  return (
+    <button className="btn-primary" type={type} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
